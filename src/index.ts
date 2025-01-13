d35c699cc6c106e88878dbc17e2d0c9d38ed722f
Shubhamxshah <ritikgujarat@gmail.com>
2025-01-13 18:32:59 +0530

    reverted index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,14 @@ import promClient from "prom-client";
 export const app = express();
 app.use(express.json());
 
-// Create and configure Registry
 const register = new promClient.Registry();
+
 register.setDefaultLabels({
   app: "express-prometheus-monitoring",
 });
 
-// Enable default metrics collection (includes CPU usage)
-promClient.collectDefaultMetrics({
-  register,
-  prefix: "node_",
-  gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5],
-});
+promClient.collectDefaultMetrics({ register });
 
-// HTTP request duration histogram
 const httpRequestDurationMicroSeconds = new promClient.Histogram({
   name: "http_request_duration_seconds",
   help: "Duration of http requests in seconds",
@@ -26,53 +20,19 @@ const httpRequestDurationMicroSeconds = new promClient.Histogram({
   buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10],
 });
 
-// Request counter for request rate
-const httpRequestsTotal = new promClient.Counter({
-  name: "http_requests_total",
-  help: "Total number of HTTP requests",
-  labelNames: ["method", "route", "code"],
-});
-
-// CPU Usage gauge (in addition to default metrics)
-const cpuUsageGauge = new promClient.Gauge({
-  name: "process_cpu_usage",
-  help: "Process CPU usage percentage",
-});
-
-// Register all metrics
 register.registerMetric(httpRequestDurationMicroSeconds);
-register.registerMetric(httpRequestsTotal);
-register.registerMetric(cpuUsageGauge);
 
-// Update CPU usage every 5 seconds
-setInterval(() => {
-  const startUsage = process.cpuUsage();
-  setTimeout(() => {
-    const endUsage = process.cpuUsage(startUsage);
-    const userCPUUsage = endUsage.user / 1000000; // Convert to seconds
-    const systemCPUUsage = endUsage.system / 1000000;
-    cpuUsageGauge.set((userCPUUsage + systemCPUUsage) * 100);
-  }, 100);
-}, 5000);
-
-// Middleware to track metrics
 app.use((req, res, next) => {
   const start = process.hrtime();
 
   res.on("finish", () => {
-    // Duration tracking
     const duration = process.hrtime(start);
     const durationInSeconds = duration[0] + duration[1] / 1e9;
-    const statusCode = res.statusCode.toString();
-    const route = req.route ? req.route.path : req.path;
 
-    // Record duration
+    const statuscode = res.statusCode.toString();
     httpRequestDurationMicroSeconds
-      .labels(req.method, route, statusCode)
+      .labels(req.method, req.route ? req.route.path : req.path, statuscode)
       .observe(durationInSeconds);
-
-    // Increment request counter
-    httpRequestsTotal.labels(req.method, route, statusCode).inc();
   });
 
   next();
