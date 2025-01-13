0a3b5ef36e97bba0a1712537e631485239fb2fdf
Shubhamxshah <ritikgujarat@gmail.com>
2025-01-14 00:53:20 +0530

    minor index change

--- src/bin.ts
+++ src/bin.ts
@@ -3,5 +3,5 @@ import { app } from ".";
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
-  console.log(`server listening on port ${PORT}`);
+  console.log(`server is listening on port ${PORT}`);
 });
