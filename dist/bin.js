4d2a66f9a8614414d65a0aeb0f8739326044c2fd
Shubhamxshah <ritikgujarat@gmail.com>
2025-02-05 15:58:02 +0530

    removed dist

--- dist/bin.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _1 = require(".");
-const PORT = process.env.PORT || 3002;
-_1.app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
-});
